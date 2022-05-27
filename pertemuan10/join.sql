7f54273ba4c4efc65cd12d36cbe0413bc3b9b145
Insan Al-Amin <54609220+insanalamin@users.noreply.github.com>
2022-05-27 08:25:00 +0700

    Create join.sql

---
+++ pertemuan10/join.sql
@@ -0,0 +1,6 @@
+CREATE TABLE mahasiswa (
+  nim VARCHAR(10) NOT NULL,
+  nama VARCHAR(80) NOT NULL
+);
+
+INSERT INTO mahasiswa (nim, nama) VALUES ('11', 'nenti'), ('12', 'norma'), ('13', 'romi'), ('14', 'nurrohman');
