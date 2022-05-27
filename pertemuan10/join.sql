60a288c00b1fbe371d2fb3e1f91aaa1095dbf4fc
Insan Al-Amin <54609220+insanalamin@users.noreply.github.com>
2022-05-27 08:28:56 +0700

    Update join.sql

--- pertemuan10/join.sql
+++ pertemuan10/join.sql
@@ -4,3 +4,11 @@ CREATE TABLE mahasiswa (
 );
 
 INSERT INTO mahasiswa (nim, nama) VALUES ('11', 'nenti'), ('12', 'norma'), ('13', 'romi'), ('14', 'nurrohman');
+
+CREATE TABLE anggota_himpunan (
+  nim VARCHAR(10) NOT NULL,
+  jabatan VARCHAR(20) NOT NULL,
+  skill_utama VARCHAR(40) NOT NULL
+);
+
+INSERT INTO anggota_himpunan (nim, jabatan, skill_utama) VALUES ('11', 'ketua', 'meneropong'), ('13', 'bendahara', 'mendamaikan'), ('14', 'koor inovasi', 'kontemplasi'), ('15', 'anggota', 'menyelinap');
