42a1ebb84b4c1e59cf38c985bac7bf5c0ff30a82
Insan Al-Amin <54609220+insanalamin@users.noreply.github.com>
2022-06-10 08:27:34 +0700

    Update webservice.php

--- pertemuan14/webservice.php
+++ pertemuan14/webservice.php
@@ -1,38 +1,38 @@
 <?php 
-	$alamatServer = "localhost";
-	$userBasisData = "root";
-	$passwordBasisData = "";
-	$namaBasisData = "project";
+  $alamatServer = "localhost";
+  $userBasisData = "root";
+  $passwordBasisData = "";
+  $namaBasisData = "project";
 	
-	header('Content-Type: application/json; charset=utf-8');
+  header('Content-Type: application/json; charset=utf-8');
 	
-	function ambilSemuaDataPenduduk($alamatServer, $userBasisData, $passwordBasisData, $namaBasisData) {
-		try {
+  function ambilSemuaDataPenduduk($alamatServer, $userBasisData, $passwordBasisData, $namaBasisData) {
+    try {
 			
-			// Coba dulu apa yang ada di sini,
+      // Coba dulu apa yang ada di sini,
 			
-			// Biasanya dikasih nama $conn 
-			$objekKoneksiBasisData = new PDO("mysql:localhost=$alamatServer;dbname=$namaBasisData", $userBasisData, $passwordBasisData);
+      // Biasanya dikasih nama $conn 
+      $objekKoneksiBasisData = new PDO("mysql:localhost=$alamatServer;dbname=$namaBasisData", $userBasisData, $passwordBasisData);
 			
-			$objekKoneksiBasisData->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $objekKoneksiBasisData->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			
-			$objekPerintah = $objekKoneksiBasisData->prepare("SELECT umur, jumlah_penduduk FROM sebaran_umur");
-			$objekPerintah->execute();
+      $objekPerintah = $objekKoneksiBasisData->prepare("SELECT umur, jumlah_penduduk FROM sebaran_umur");
+      $objekPerintah->execute();
 			
-			$hasilQuery = $objekPerintah->fetchAll();
+      $hasilQuery = $objekPerintah->fetchAll();
 			
-			echo json_encode($hasilQuery);
+      echo json_encode($hasilQuery);
 			
-			// Perintah query
+      // Perintah query
 			
-			$objekKoneksiBasisData = null;
+      $objekKoneksiBasisData = null;
 			
-		} catch(PDOException $e) {
-			// Kalo gagal, jalankan yang dibawah ini
+    } catch(PDOException $e) {
+      // Kalo gagal, jalankan yang dibawah ini
 			
-			echo "Sebab gagalnya: " . $e->getMessage();
-		}
-	}
+      echo "Sebab gagalnya: " . $e->getMessage();
+    }
+  }
 	
-	ambilSemuaDataPenduduk($alamatServer, $userBasisData, $passwordBasisData, $namaBasisData);
+  ambilSemuaDataPenduduk($alamatServer, $userBasisData, $passwordBasisData, $namaBasisData);
 ?>
