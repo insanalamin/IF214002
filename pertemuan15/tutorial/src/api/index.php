fdeab6acf5e518127bf1c23e81c21573c1260267
Insan Al-Amin <54609220+insanalamin@users.noreply.github.com>
2022-06-25 16:44:38 +0700

    Update index.php

--- pertemuan15/tutorial/src/api/index.php
+++ pertemuan15/tutorial/src/api/index.php
@@ -1,7 +1,8 @@
 <?php
 
-require_once($_SERVER['DOCUMENT_ROOT']."/api/router.php");
+require_once($_SERVER['DOCUMENT_ROOT']."/api/route.php");
 require_once($_SERVER['DOCUMENT_ROOT']."/api/handlers/get_all_penduduk.php");
+require_once($_SERVER['DOCUMENT_ROOT']."/api/handlers/post_penduduk.php");
 require_once($_SERVER['DOCUMENT_ROOT']."/api/handlers/get_penduduk_by_nik.php");
 
 header("Access-Control-Allow-Origin: *");
@@ -9,7 +10,20 @@ header("Access-Control-Allow-Credentials: true");
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE, PUT');
 header('Content-Type: application/json; charset=utf-8');
 
-$router = new Router();
-$router->addRoute('GET', '/penduduk', function(){getAllPenduduk();});
-$router->addRoute('GET', '/penduduk/:nik', function(){getPendudukByNIK($nik);});
-$router->doRouting(); 
+$method = $_SERVER['REQUEST_METHOD'];
+
+Route::add('/api/index.php/penduduk', function() {
+  getAllPenduduk();
+}, 'get');
+
+Route::add('/api/index.php/penduduk', function() {
+  // POST content: $_POST
+  // JSON POST content: file_get_contents('php://input')
+  postPenduduk();
+}, 'post');
+
+Route::add('/api/index.php/penduduk/([0-9]{16})', function($nik) {
+  getPendudukByNIK("5445");
+}, 'get');
+
+Route::run('/');
